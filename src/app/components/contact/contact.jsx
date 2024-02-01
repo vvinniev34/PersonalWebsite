ef5b816178144fc65d68462bc9ded8f69024be62
Vincent Liu <vvinniev34@gmail.com>
2024-02-01 01:19:15 -0600

    mobile contact footer

--- src/app/components/contact/contact.jsx
+++ src/app/components/contact/contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
                 <>
                     <div className='font-semibold'>Let's connect</div>
                     <div>Get in touch for opportunites or just to say hi! 👋</div>
-                    <div className="flex width" style={{width:"40%"}}>
+                    <div className="flex" style={{width:"20rem", maxWidth:"fit-content"}/*{width:"40%"}*/}>
                         <div className="flex flex-col ">
                         <div className="flex m-5" style={{width:"90%"}}>
                             <div className={styles.contactSVG} onClick={handleGithubClick}>
@@ -80,7 +80,7 @@ const Contact = () => {
                             </div>
                         </div>
 
-                        <SecondaryPageDivider/>
+                        <SecondaryPageDivider dark={true}/>
 
                         <div className="flex m-5" style={{width:"90%"}}>
                             <div className={styles.contactSVG}>
--- src/app/components/pagedivider/secondarypagedivider.jsx
+++ src/app/components/pagedivider/secondarypagedivider.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const SecondaryPageDivider = (props) => {
-    const { marginTop = 0, marginBottom = 0 } = props;
+    const { marginTop = 0, marginBottom = 0, dark=false } = props;
     return (
         // <div className={`bg-black w-full`} style={{ height:"1px", marginTop:`${marginTop}%`, marginBottom:`${marginBottom}%`, width:"100%"}}>
         // </div>
-        <hr style={{borderTop:"1px solid black"}}></hr>
+        <hr style={{borderTop:`1px solid ${dark ? "white" : "black"}`}}></hr>
     );
 }
 
