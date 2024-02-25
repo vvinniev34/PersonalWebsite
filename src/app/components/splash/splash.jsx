870fb0ad232776717bcdbd89a85e5031994cc862
Vincent Liu <vvinniev34@gmail.com>
2024-02-25 03:46:07 -0600

    added image alt name

--- src/app/components/splash/splash.jsx
+++ src/app/components/splash/splash.jsx
@@ -24,7 +24,7 @@ const Splash = () => {
 			<div style={{ position: "fixed", width:"100%", height:"100vh", zIndex:-1 }}>
 				<Image 
 					src="/sky_ocean_surface_brightened.png" 
-					description="" 
+					alt="bright sky over ocean surface" 
 					width="100vh" 
 					height="100vh" 
 					layout="fill" 
