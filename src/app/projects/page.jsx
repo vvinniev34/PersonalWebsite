9b31b0a4493288de1596a321eb8712ebe063ce55
Vincent Liu <vvinniev34@gmail.com>
2024-03-06 02:02:13 -0600

    image width changes

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -6,33 +6,31 @@ import StackSVG from "../components/projectcontainer/svgs.jsx"
 import Image from "next/image";
 import "../globals.css";
 
-const colors = ["darkorange", "burlywood", "indianred", "tan", "cadetblue", "lightsalmon", "darkcyan", "chocolate"]
-
 const ProjectPage = () => {
 
     const projects = [
         <ProjectContainer 
             title={"RedditReels"} 
             image="/projects/reddit.jpg"
-            color={colors[2]}
+            color={"indianred"}
             description={<div>Automatic short-form video content generator and uploader for TikTok, YouTube, and Instagram.<br/>About 30,000,000 annual views.</div>}
         />, 
         <ProjectContainer 
             title={"User-Level Thread Library"}
             svg={<StackSVG/>}
-            color={colors[2]}
+            color={"indianred"}
             description={<div>User-level thread library and API.<br/>Contains vital functions such as thread creation, scheduling, synchronization, and termination.</div>}
         />, 
         <ProjectContainer 
             title={"VoiceNotes"}
             image="/projects/voicenotes.jpg"
-            color={colors[2]}
+            color={"indianred"}
             description={<div>Fully voice controlled note-taking app for hands-free use.</div>}
         />, 
         <ProjectContainer 
             title={"GroupFinder"} 
             image="/projects/group.png"
-            color={colors[2]}
+            color={"indianred"}
             description={<div>Clusters user-submitted data based upon similarity.<br/>Uses a parallelized implementation of a k-means clustering ML algorithm.</div>}
         />
     ]
