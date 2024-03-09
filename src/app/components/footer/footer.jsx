16003f369557c7f9fc2b315eb94f5e9871f49248
Vincent Liu <vvinniev34@gmail.com>
2024-03-09 02:23:17 -0600

    footer links

--- src/app/components/footer/footer.jsx
+++ src/app/components/footer/footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Contact from '../contact/contact'
-// import styles from "./footer.module.css"
+import styles from "./footer.module.css"
 
 export default function Footer(){
     return (
@@ -16,7 +16,7 @@ export default function Footer(){
                 style={{ paddingBottom:'1.25rem', background: 'rgba(0, 0, 0, 0.2)' }}
             >
                 <p>Thanks for checking out my portfolio!</p>
-                <p>Designed in <span className='artifactWord'>Figma</span>, coded in <span className='artifactWord'>Visual Studio Code</span>, built using <span className='artifactWord'>Next.js</span> & <span className='artifactWord'>Tailwind CSS</span>, deployed with <span className='artifactWord'>Vercel</span>, and most importantly, built by me! 😊</p>
+                <p>Designed in <a className={styles.textHover} href='https://figma.com' target="_blank" rel="noopener noreferrer">Figma</a>, coded in <a className={styles.textHover} href='https://code.visualstudio.com' target="_blank" rel="noopener noreferrer">Visual Studio Code</a>, built using <a className={styles.textHover} href='https://nextjs.org' target="_blank" rel="noopener noreferrer">Next.js</a> & <a className={styles.textHover} href='https://tailwindcss.com' target="_blank" rel="noopener noreferrer">Tailwind CSS</a>, deployed with <a className={styles.textHover} href='https://vercel.com' target="_blank" rel="noopener noreferrer">Vercel</a>, and most importantly, built by me! 😊</p>
             </div>
         </div>
     );
