36acb0db23517b5a8af6f057b06b9a63f123d623
Vincent Liu <vvinniev34@gmail.com>
2024-05-24 19:44:15 -0500

    skills bug fix

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 import React from "react";
 import TertiaryHeader from "../components/headers/tertiaryheader";
-import { useInView } from 'react-intersection-observer';
-import styles from './page.module.css'
+import { useInView } from "react-intersection-observer";
+import styles from "./page.module.css";
 
 const SkillsPage = () => {
   const languages = (
@@ -18,7 +18,7 @@ const SkillsPage = () => {
       <li>CSS</li>
       <li>SQL</li>
     </ul>
-  )
+  );
   const frameworks = (
     <ul>
       <li>React.js</li>
@@ -27,7 +27,7 @@ const SkillsPage = () => {
       <li>Django</li>
       <li>Node.js</li>
     </ul>
-  )
+  );
 
   const tools = (
     <ul>
@@ -40,7 +40,7 @@ const SkillsPage = () => {
       <li>Docker</li>
       <li>Selenium</li>
     </ul>
-  )
+  );
 
   const databases = (
     <ul>
@@ -50,68 +50,100 @@ const SkillsPage = () => {
       <li>Firebase</li>
       <li>CosmosDB</li>
     </ul>
-  )
+  );
 
   const { ref, inView } = useInView({
     triggerOnce: true,
   });
-  
+
   return (
-    <section 
-      className="section flex flex-col items-center justify-between" 
-      style={{paddingBottom:'4.5rem', paddingTop:'4.5rem', position:"relative", overflow:"hidden"}}
+    <section
+      className="section flex flex-col items-center justify-between"
+      style={{
+        paddingBottom: "4.5rem",
+        paddingTop: "4.5rem",
+        position: "relative",
+        overflow: "hidden",
+      }}
       id="skills"
     >
-      <div className="flex space-y-20 w-full featuredRightPadding" style={{ zIndex:30 }}>
-          <div className="mx-auto w-full max-w-[100rem] rounded-md overflow-hidden">
-              <div 
-                  className="w-[100%] flex items-center justify-center " 
-                  style={{paddingTop:"2.5%", paddingBottom:"2.5%"}}
+      <div
+        className="flex space-y-20 w-full featuredRightPadding"
+        style={{ zIndex: 30 }}
+      >
+        <div className="mx-auto w-full max-w-[100rem] rounded-md overflow-hidden">
+          <div
+            className="w-[100%] flex items-center justify-center "
+            style={{ paddingTop: "2.5%", paddingBottom: "2.5%" }}
+          >
+            <div
+              ref={ref}
+              className={`w-[90%] flex flex-row flex-wrap justify-between overflow-hidden`}
+              style={{
+                paddingTop: "1.25rem",
+                paddingBottom: "1.25rem",
+                paddingLeft: "0.75rem",
+                paddingRight: "0.75rem",
+              }}
+            >
+              <div
+                className={`overflow-hidden`}
+                style={{ padding: "0.5rem", paddingBottom: "2rem" }}
               >
-                  <div 
-                    ref={ref} 
-                    className={`w-[90%] flex flex-row flex-wrap justify-between overflow-hidden`} 
-                    style={{paddingTop:'1.25rem', paddingBottom:'1.25rem', paddingLeft:'0.75rem', paddingRight:'0.75rem'}}
-                  >
-                      <div 
-                        className={`overflow-hidden`}
-                        style={{padding:"0.5rem", paddingBottom:'2rem'}}
-                      >
-                        <div className={`${inView ? styles.dropIn1 : ""}`}>
-                          <TertiaryHeader title={"Languages"} description={languages} color={"#f6f7f2"} descriptionColor={"#f6f7f2"}></TertiaryHeader>
-                        </div>
-                      </div>
-                      <div 
-                        className={`overflow-hidden`}
-                        style={{padding:"0.5rem", paddingBottom:'2rem'}}
-                      >
-                        <div className={`${inView ? styles.dropIn2 : ""}`}>
-                          <TertiaryHeader title={"Frameworks"} description={frameworks} color={"#f6f7f2"} descriptionColor={"#f6f7f2"}></TertiaryHeader>
-                        </div>
-                      </div>
-                      <div
-                        className={`overflow-hidden`} 
-                        style={{padding:"0.5rem", paddingBottom:'2rem'}}
-                      >
-                        <div className={`${inView ? styles.dropIn3 : ""}`}>
-                          <TertiaryHeader title={"Tools"} description={tools} color={"#f6f7f2"} descriptionColor={"#f6f7f2"}></TertiaryHeader>
-                        </div>
-                      </div>
-                      <div 
-                        className={`overflow-hidden`} 
-                        style={{padding:"0.5rem", paddingBottom:'2rem'}}
-                      >
-                        <div className={`${inView ? styles.dropIn4 : ""}`}>
-                          <TertiaryHeader title={"Databases"} description={databases} color={"#f6f7f2"} descriptionColor={"#f6f7f2"}></TertiaryHeader>
-                        </div>
-                      </div>
-                  </div>
+                <div className={`${inView ? styles.dropIn1 : "opacity-0"}`}>
+                  <TertiaryHeader
+                    title={"Languages"}
+                    description={languages}
+                    color={"#f6f7f2"}
+                    descriptionColor={"#f6f7f2"}
+                  ></TertiaryHeader>
+                </div>
               </div>
+              <div
+                className={`overflow-hidden`}
+                style={{ padding: "0.5rem", paddingBottom: "2rem" }}
+              >
+                <div className={`${inView ? styles.dropIn2 : "opacity-0"}`}>
+                  <TertiaryHeader
+                    title={"Frameworks"}
+                    description={frameworks}
+                    color={"#f6f7f2"}
+                    descriptionColor={"#f6f7f2"}
+                  ></TertiaryHeader>
+                </div>
+              </div>
+              <div
+                className={`overflow-hidden`}
+                style={{ padding: "0.5rem", paddingBottom: "2rem" }}
+              >
+                <div className={`${inView ? styles.dropIn3 : "opacity-0"}`}>
+                  <TertiaryHeader
+                    title={"Tools"}
+                    description={tools}
+                    color={"#f6f7f2"}
+                    descriptionColor={"#f6f7f2"}
+                  ></TertiaryHeader>
+                </div>
+              </div>
+              <div
+                className={`overflow-hidden`}
+                style={{ padding: "0.5rem", paddingBottom: "2rem" }}
+              >
+                <div className={`${inView ? styles.dropIn4 : "opacity-0"}`}>
+                  <TertiaryHeader
+                    title={"Databases"}
+                    description={databases}
+                    color={"#f6f7f2"}
+                    descriptionColor={"#f6f7f2"}
+                  ></TertiaryHeader>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
 export default SkillsPage;
