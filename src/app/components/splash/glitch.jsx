6ff897af60be34c387e3e5f7090d9b798acb7e27
Vincent Liu <vvinniev34@gmail.com>
2024-05-30 00:15:40 -0500

    splash screen color fix

--- src/app/components/splash/glitch.jsx
+++ src/app/components/splash/glitch.jsx
@@ -51,7 +51,7 @@ export default function Glitch() {
 
 	return (
 		<h2 
-			className="font-mono text-black hover:text-gray-500 text-lg sm:text-xl md:text-2xl" 
+			className="font-mono text-black hover:text-[#574e4e] text-lg sm:text-xl md:text-2xl" 
 			onClick={() => scrollDown()}
 			style={{cursor:"pointer"}}
 		>
